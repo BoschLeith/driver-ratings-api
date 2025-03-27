a87c99118d4f001ea6d61a3ed0c1b17bdd4b3a8f
BoschLeith <bosch.leith@gmail.com>
2025-03-27 14:31:22 -0500

    Add DELETE method to CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const CORS_ORIGIN = isDev
 app.use(
   cors({
     origin: CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
